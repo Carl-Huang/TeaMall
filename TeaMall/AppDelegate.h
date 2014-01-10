9da2dd49e8ca0d6ba3d221cab87fb76753bae08b
Carl <Carl@Carls-MacBook-Pro.local>
2014-01-10 16:01:13 +0800

    init project

---
+++ TeaMall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TeaMall
+//
+//  Created by Carl on 14-1-10.
+//  Copyright (c) 2014年 helloworld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
