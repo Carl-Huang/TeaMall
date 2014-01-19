8077658ff46a6078fae09d5f66e508c41fb79ae0
vedon <vedon.fu@gmail.com>
2014-01-19 17:10:08 +0800

    添加afnet,magical record.etc

---
+++ TeaMall/Model/LoginInfo.h
@@ -0,0 +1,13 @@
+//
+//  LoginInfo.h
+//  TeaMall
+//
+//  Created by vedon on 19/1/14.
+//  Copyright (c) 2014 helloworld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginInfo : NSObject
+
+@end
